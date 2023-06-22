80c1310abaa18ae4e11f76cfef3bba46500b1ffe
caiosmaia <37799921+caiosmaia@users.noreply.github.com>
2023-06-21 23:20:01 -0300

    Update Composicao.java

--- teste/Composicao.java
+++ teste/Composicao.java
@@ -2,12 +2,22 @@ package teste;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Composicao {
 	private int numero;
     private Stack<Vagao> vagoes;
+    
+    
+    
+    private boolean noTerminal;
+    private boolean noPontoInterconexao;
+    private boolean EmMovimento;
     
     public static final String[] conteudosPossiveisRecebeR1 = {"amendoim", "feijao", "milho", "soja", "trigo"};
 	public static final String[] conteudosPossiveisRecebeR2 = {"cobre", "ferro", "magnesita", "niquel"};
@@ -16,11 +26,29 @@ public class Composicao {
     public Composicao(int numero) {
         this.numero = numero;
         this.vagoes = new Stack<>();
+        this.noTerminal = true;
+        this.noPontoInterconexao = false;
+        this.EmMovimento=false;
+      
+      
     }
 
-    public int getNumero() {
+
+
+	public int getNumero() {
         return numero;
     }
+    
+    public boolean getEmMovimento() {
+		return EmMovimento;
+    	
+    }
+    
+    public void setEmMovimento(boolean EmMovimento) {
+		this.EmMovimento=EmMovimento;
+    	
+    }
+
 
     public Stack<Vagao> getVagoes() {
         return vagoes;
@@ -33,11 +61,15 @@ public class Composicao {
     }
 
     public void adicionarCarga(String identificador, String conteudo, int quantidade) {
-        for (Vagao vagao : vagoes) {
+        for (Vagao vagao : vagoes){
+        	
             if (vagao.getNome().equals(identificador)) {
+            	if(vagao.isVazio()) {
                 vagao.adicionarCarga(quantidade, conteudo, quantidade);
                 System.out.println("Carga de " + quantidade + "kg de " + conteudo + " adicionada ao vagão " +
                         vagao.getNome() + " na composição " + numero + ".");
+            	}else
+            		System.out.println("Vagao já está cheio");
                 return;
             }
         }
@@ -74,4 +106,108 @@ public class Composicao {
         }
         System.out.println("Vagão " + identificador + " não encontrado na composição " + numero + ".");
     }
+    
+    public void transferirVagoes(Composicao outraComposicao) {
+        if (noTerminal || outraComposicao.noTerminal) {
+            System.out.println("Não é possível fazer a transferência de vagões no terminal.");
+            return;
+        }
+
+        if (noPontoInterconexao && outraComposicao.noPontoInterconexao) {
+           
+            Stack<Vagao> vagoesAtual = new Stack<>();
+            Stack<Vagao> vagoesOutra = new Stack<>();
+
+            while (!vagoes.isEmpty()) {
+                vagoesAtual.push(vagoes.pop());
+            }
+            while (!outraComposicao.vagoes.isEmpty()) {
+                vagoesOutra.push(outraComposicao.vagoes.pop());
+            }
+
+            while (!vagoesAtual.isEmpty()) {
+                outraComposicao.vagoes.push(vagoesAtual.pop());
+            }
+            while (!vagoesOutra.isEmpty()) {
+                vagoes.push(vagoesOutra.pop());
+            }
+
+            System.out.println("Transferência de vagões entre a composição " + numero + " e a composição " +
+                    outraComposicao.numero + " realizada com sucesso.");
+            return;
+        }
+
+        
+    }
+
+    public void setNoTerminal(boolean noTerminal) {
+        this.noTerminal = noTerminal;
+        if (noTerminal) {
+            noPontoInterconexao = false;
+            System.out.println("Composição " + numero + " está no terminal: Embarque/Desembarque.");
+        }
+    }
+    
+    public boolean isNoTerminal() {
+        return noTerminal;
+    }
+
+    public void setNoPontoInterconexao(boolean noPontoInterconexao) {
+        this.noPontoInterconexao = noPontoInterconexao;
+        if (noPontoInterconexao) {
+            noTerminal = false;
+            System.out.println("Composição " + numero + " está no ponto de interconexão: Transferência de vagões.");
+        }
+    }
+    
+   
+
+    
+    public void viagemComposicao1() {
+        noTerminal = false;
+        noPontoInterconexao = false;
+
+        for (int i = 10; i > 0; i--) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        noTerminal = false;
+        noPontoInterconexao = true;
+
+        System.out.println("\nComposição 1 chegou ao Ponto de Interconexão.");
+    }
+    
+    public void viagemComposicao2() {
+        noTerminal = true;
+        noPontoInterconexao = false;
+
+        for (int i = 10; i > 0; i--) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        noTerminal = false;
+        noPontoInterconexao = true;
+
+        System.out.println("Composição 2 chegou ao Ponto de Interconexão.");
+    }
+    
+    private List<String> getConteudosPossiveis(int composicao) {
+        if (composicao == 1) {
+            return Arrays.asList("amendoim", "feijao", "milho", "soja", "trigo");
+        } else if (composicao == 2) {
+            return Arrays.asList("cobre", "ferro", "magnesita", "niquel");
+        } else {
+            return Collections.emptyList();
+        }
+    }
+    
+    
 }
